b7ce48b8ddb8626e558151586095deb517f07225
User <test@example.com>
2026-05-30 17:57:02 +0530

    Update server connection configuration and ignore APK

--- frontend/lib/utils/constants.dart
+++ frontend/lib/utils/constants.dart
@@ -39,6 +39,7 @@ class AppColors {
 
 class ApiConstants {
   static const List<String> knownBaseUrls = [
+    'http://192.168.1.8:5000',
     'https://finwise-backend-dtkb.onrender.com',
     'http://192.168.1.5:5000',
     'http://192.168.137.104:5000',
